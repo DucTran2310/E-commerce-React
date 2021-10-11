d96004110d66d2bb7e790d30699be18b050151a4
DucTran2310 <18520021@gm.uit.edu.vn>
2021-10-11 21:54:16 +0700

    Update footer component

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,7 +1,109 @@
 import React from "react";
 
+import { Link } from "react-router-dom";
+
+import Grid from "../grid/index";
+
+import logo from "../../assets/images/Logo-2.png";
+
+const footerAboutLinks = [
+    {
+        display: "Giới thiệu",
+        path: "/about",
+    },
+    {
+        display: "Liên hệ",
+        path: "/about",
+    },
+    {
+        display: "Tuyển dụng",
+        path: "/about",
+    },
+    {
+        display: "Tin tức",
+        path: "/about",
+    },
+    {
+        display: "Hệ thống cửa hàng",
+        path: "/about",
+    },
+];
+
+const footerCustomerLinks = [
+    {
+        display: "Chính sách đổi trả",
+        path: "/about",
+    },
+    {
+        display: "Chính sách bảo hành",
+        path: "/about",
+    },
+    {
+        display: "Chính sách hoàn tiền",
+        path: "/about",
+    },
+];
+
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer>
+            <div className="container">
+                <Grid col={4} mdCol={2} smCol={2} gap={10}>
+                    <div>
+                        <div className="footer__title">Tổng đài hỗ trợ</div>
+                        <div className="footer__content">
+                            <p>
+                                Liên hệ đặt hàng <strong>012345678</strong>
+                            </p>
+                            <p>
+                                Thắc mắc đơn hàng <strong>012345678</strong>
+                            </p>
+                            <p>
+                                Góp ý, khiếu nại <strong>012345678</strong>
+                            </p>
+                        </div>
+                    </div>
+                    <div>
+                        <div className="footer__title">Về Yolo</div>
+                        <div className="footer__content">
+                            {footerAboutLinks.map((item, index) => (
+                                <p key={index}>
+                                    <Link to={item.path}>{item.display}</Link>
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                    <div>
+                        <div className="footer__title">Chăm sóc khách hàng</div>
+                        <div className="footer__content">
+                            {footerCustomerLinks.map((item, index) => (
+                                <p key={index}>
+                                    <Link to={item.path}>{item.display}</Link>
+                                </p>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="footer__about">
+                        <p>
+                            <Link to="/">
+                                <img
+                                    src={logo}
+                                    className="footer__logo"
+                                    alt="Yolo-ClothStore"
+                                />
+                            </Link>
+                        </p>
+                        <p>
+                            Hướng đến mục tiêu mang lại niềm vui ăn mặc mới mỗi
+                            ngày cho hàng triệu người tiêu dùng Việt. Hãy cùng
+                            Yolo hướng đến một cuộc sống năng động, tích cực
+                            hơn.
+                        </p>
+                    </div>
+                </Grid>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
