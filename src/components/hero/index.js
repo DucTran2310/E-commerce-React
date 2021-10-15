8e5fe27b336aa1f21e1e3f029f92a300267ece2e
DucTran2310 <18520021@gm.uit.edu.vn>
2021-10-15 22:16:18 +0700

    Add hero component

---
+++ src/components/hero/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Carousel from "react-bootstrap/Carousel";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Hero = ({ start }) => {
+    return (
+        <Carousel fade>
+            {start.map((item, index) => (
+                <Carousel.Item key={index}>
+                    <img
+                        className="d-block w-100"
+                        src={item}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+            ))}
+        </Carousel>
+    );
+};
+
+export default Hero;
